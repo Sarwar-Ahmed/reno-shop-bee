429c6cb50347dda564ff798aea054ff856ce2d8a
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2022-02-03 01:37:00 +0600

    components creating done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeBanner from './HomeBanner/HomeBanner';
+import ProductsCards from './ProductsCards/ProductsCards';
+
+const Home = () => {
+    return (
+        <div>
+            <HomeBanner/>
+            <ProductsCards/>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Home/ProductsCards/ProductsCards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsCards = () => {
+    return (
+        <div>
+            <h1>ProductsCards</h1>
+        </div>
+    );
+};
+
+export default ProductsCards;
