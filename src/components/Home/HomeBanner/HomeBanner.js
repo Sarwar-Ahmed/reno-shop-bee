fff5d7d2f1dee2ef0ad23e10e6f33008df4cd6d3
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2022-02-04 19:02:42 +0600

    Home Banner done

--- src/components/Home/HomeBanner/HomeBanner.js
+++ src/components/Home/HomeBanner/HomeBanner.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import './HomeBanner.css';
 
 const HomeBanner = () => {
     return (
-        <div>
-            <h1>HomeBanner</h1>
+        <div className='homeBanner row text-start p-5'>
+            <div className='col-md-6 p-5 mb-5 mt-5'>
+                <h1>THE BEST <br /><span style={{color: '#3CB878'}}>WOOCOMMMERCE</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi est inventore architecto eum corrupti nesciunt?</p>
+                <button className="btn btn-dark pl-5 pr-5">BUY NOW</button>
+            </div>
         </div>
     );
 };
